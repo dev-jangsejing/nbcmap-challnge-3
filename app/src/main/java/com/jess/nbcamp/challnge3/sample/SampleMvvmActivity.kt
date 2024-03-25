21bedad8ec4440025f0098937d521d033465c7ac
JangSejin <sejin-011@hanmail.net>
2024-03-25 20:01:51 +0900

    mvvm sample

---
+++ app/src/main/java/com/jess/nbcamp/challnge3/sample/SampleMvvmActivity.kt
@@ -0,0 +1,45 @@
+package com.jess.nbcamp.challnge3.sample
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.jess.nbcamp.challnge3.R
+
+class SampleMvvmActivity : AppCompatActivity() {
+
+    private val tvTest: TextView by lazy {
+        findViewById(R.id.tv_test)
+    }
+
+    private val btTest: Button by lazy {
+        findViewById(R.id.bt_test)
+    }
+
+    private val viewModel: SampleMvvmViewModel by lazy {
+        ViewModelProvider(this)[SampleMvvmViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        initView()
+        initViewModel()
+
+    }
+
+    private fun initView() {
+        btTest.setOnClickListener {
+            viewModel.onClickTest("jess")
+        }
+    }
+
+    private fun initViewModel() {
+        viewModel.event.observe(this@SampleMvvmActivity) {
+            tvTest.text = it
+        }
+    }
+
+}
---
+++ app/src/main/java/com/jess/nbcamp/challnge3/sample/SampleMvvmViewModel.kt
@@ -0,0 +1,18 @@
+package com.jess.nbcamp.challnge3.sample
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SampleMvvmViewModel : ViewModel() {
+
+    private val _event: MutableLiveData<String> = MutableLiveData()
+    val event: LiveData<String> get() = _event
+
+    fun onClickTest(action: String) {
+        // 이런저런 데이터 처리, 이것저것 처리 등등...
+
+        val result = "result $action"
+        _event.value = result
+    }
+}
