39f0b4dce56bb5ec333002db3012f8df575e4337
jangsejin <sejin-011@hanmail.net>
2024-04-08 18:58:15 +0900

    회원가입 유효성 검사

--- app/src/main/java/com/jess/nbcamp/challnge3/practice/SignUpViewModel.kt
+++ app/src/main/java/com/jess/nbcamp/challnge3/practice/SignUpViewModel.kt
@@ -3,30 +3,75 @@ package com.jess.nbcamp.challnge3.practice
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jess.nbcamp.challnge3.practice.SignUpValidExtension.includeAt
+import com.jess.nbcamp.challnge3.practice.SignUpValidExtension.includeSpecialCharacters
+import com.jess.nbcamp.challnge3.practice.SignUpValidExtension.includeUpperCase
 
 class SignUpViewModel : ViewModel() {
+
+    // user
+    private val _errorUiState: MutableLiveData<SignUpErrorUiState> =
+        MutableLiveData(SignUpErrorUiState.init())
+    val errorUiState: LiveData<SignUpErrorUiState> get() = _errorUiState
 
     private val _event: MutableLiveData<SignUpEvent> = MutableLiveData()
     val event: LiveData<SignUpEvent> get() = _event
 
-    /**
-     * 회원가입 버튼을 클릭합니다.
-     *
-     * @param name ...
-     */
-    fun onClickSignUp(
-        name: String,
-        id: String,
-        password: String,
-    ) {
-        // 회원의 정보를 가져와서 서버로 데이터를 보낸 후 이것저것 처리함
-        _event.value = SignUpEvent.SignUpSuccess(
-            SignUpMember(
-                name,
-                id,
-                password
-            )
+    fun checkValidName(text: String) {
+        _errorUiState.value = errorUiState.value?.copy(
+            name = if (text.isBlank()) {
+                SignUpValidUiState.Name
+            } else {
+                SignUpValidUiState.Valid
+            }
         )
+    }
 
+    fun checkValidEmail(text: String) {
+        _errorUiState.value = errorUiState.value?.copy(
+            emailId = when {
+                text.isBlank() -> SignUpValidUiState.EmailBlank
+                text.includeAt() -> SignUpValidUiState.EmailIncludeAt
+                else -> SignUpValidUiState.Valid
+            }
+        )
+    }
+
+    fun checkValidPasswordInput(text: String) {
+        _errorUiState.value = errorUiState.value?.copy(
+            passwordInput = when {
+                text.length < 10 -> SignUpValidUiState.PasswordInputLength
+
+                text.includeSpecialCharacters().not() ->
+                    SignUpValidUiState.PasswordInputSpecialCharacters
+
+                text.includeUpperCase().not() -> SignUpValidUiState.PasswordInputUpperCase
+
+                else -> SignUpValidUiState.Valid
+            }
+        )
+    }
+
+    fun checkValidPasswordConfirm(
+        text: String,
+        confirm: String
+    ) {
+        _errorUiState.value = errorUiState.value?.copy(
+            passwordConfirm = if (text != confirm) {
+                SignUpValidUiState.PasswordConfirm
+            } else {
+                SignUpValidUiState.Valid
+            }
+        )
+    }
+
+    private fun isConfirmButtonEnable() = errorUiState.value?.let { state ->
+        _errorUiState.value?.copy(
+            enabled = state.name == SignUpValidUiState.Valid
+                    && state.emailId == SignUpValidUiState.Valid
+                    && state.emailService == SignUpValidUiState.Valid
+                    && state.passwordInput == SignUpValidUiState.Valid
+                    && state.passwordConfirm == SignUpValidUiState.Valid
+        )
     }
 }
