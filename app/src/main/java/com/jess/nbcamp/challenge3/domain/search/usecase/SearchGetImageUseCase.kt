2fd50b067b0e2016fdda707c3d05b246305a523d
jangsejin <sejin-011@hanmail.net>
2024-05-12 18:43:02 +0900

    ViewModelScoped 추가

--- app/src/main/java/com/jess/nbcamp/challenge3/domain/search/usecase/SearchGetImageUseCase.kt
+++ app/src/main/java/com/jess/nbcamp/challenge3/domain/search/usecase/SearchGetImageUseCase.kt
@@ -3,8 +3,10 @@ package com.jess.nbcamp.challenge3.domain.search.usecase
 import com.jess.nbcamp.challenge3.domain.search.model.ImageDocumentEntity
 import com.jess.nbcamp.challenge3.domain.search.model.SearchEntity
 import com.jess.nbcamp.challenge3.domain.search.repository.SearchRepository
+import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
+@ViewModelScoped
 class SearchGetImageUseCase @Inject constructor(
     private val repository: SearchRepository
 ) {
--- app/src/main/java/com/jess/nbcamp/challenge3/presentation/search/di/SearchViewModelComponentModule.kt
+++ app/src/main/java/com/jess/nbcamp/challenge3/presentation/search/di/SearchViewModelComponentModule.kt
@@ -6,11 +6,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @InstallIn(ViewModelComponent::class)
 @Module
 abstract class BindModule {
 
+    @ViewModelScoped
     @Binds
     abstract fun bindSearchRepository(
         repository: SearchRepositoryImpl
